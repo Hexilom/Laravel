8334ca2d1b78d85e3122fed36c696d11615ddc25
guille <chiari_guille@live.com>
2024-02-21 12:08:20 -0300

    hecho clase inertia

---
+++ inertiaexample/resources/js/Pages/ExampleInertia.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ counter }}</h1>
+    <button @click="increment">increment</button>
+    <NavLink :href="route('login')">login</NavLink>
+</template>
+
+<script setup>
+    
+    import { ref } from "vue";
+    import NavLink from '../Components/NavLink.vue';
+    const counter = ref(0)
+    const increment = () => {
+        counter.value++
+    }
+</script>
+
+<style scoped>
+</style>
